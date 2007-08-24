68eab2b3651230c4a02926a5635506f5ccf5a946
Gabriel Rockefeller <gaber@lanl.gov>
2007-08-24 14:07:21 -0600

    Changes from cluster to bardeen: Fix_dt() behavior
    dtlongvote = 0 requires tbad > tlow_cut (not tbad >= tlow_cut).
    
    (cherry picked from commit c97a1d4ad129a8314783596df834e47c206ee681)

--- bardeen/main.c
+++ bardeen/main.c
@@ -2289,7 +2289,7 @@ Fix_dt(float *dt, float dt_max, int tlow_cut, float tmin, int tbad,
     } else if (tbad > tlow_cut/2) {
 	dtlongvote--;
     }
-    if (tbad >= tlow_cut || limit_high >= tlow_cut || limit_low >= tlow_cut ) {
+    if (tbad > tlow_cut || limit_high > 0 || limit_low > 0 ) {
 	dtlongvote = 0;
 	dtshortvote++;
     }
