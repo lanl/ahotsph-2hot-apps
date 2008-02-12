20f3eb9d9366cdecd3c2a3d847ae8ddb964b1220
Gabriel Rockefeller <gaber@lanl.gov>
2008-02-12 10:13:57 -0700

    SPHOutput now copies dq to output_btab structure

--- snsphforgabe/main.c
+++ snsphforgabe/main.c
@@ -1959,6 +1959,7 @@ static void SPHOutput(SPHbody *btab, int nobj, const char *outnamebase, int iter
 	output_btab[i].phi = btab[i].phi;
 	output_btab[i].dt = btab[i].dt;
 #endif
+	output_btab[i].dq = btab[i].dq;
 	output_btab[i].nbrs = btab[i].nbrs;
 	output_btab[i].ident = btab[i].ident;
     }
